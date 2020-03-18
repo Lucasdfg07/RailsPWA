7ac2ae4928145d0ac35cb197160ab596e9f94576
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-03-18 16:27:33 -0300

    my first pwa

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require jquery
+//= require jquery_ujs
+//= require materialize-sprockets
+//= require serviceworker-companion
+        
